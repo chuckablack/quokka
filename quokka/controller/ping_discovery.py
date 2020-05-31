1990ffeba525ff933083a0f327aefea173d34d84
chuck <chuck.a.black@gmail.com>
2020-05-30 21:21:33 -0700

    adding mac to host table via arp

--- quokka/controller/ping_discovery.py
+++ quokka/controller/ping_discovery.py
@@ -5,8 +5,26 @@ from datetime import datetime
 
 from quokka.models.apis import set_host
 
+from python_arptable import get_arp_table
+from pprint import pprint
+
+
+def learn_mac_addresses():
+    arp_table_list = get_arp_table()
+
+    arp_table = dict()
+    for arp_entry in arp_table_list:
+
+        if arp_entry["HW address"] != "00:00:00:00:00:00":
+            arp_table[arp_entry["IP address"]] = arp_entry["HW address"]
+
+    pprint(arp_table)
+    return arp_table
+
 
 def discover():
+
+    mac_addresses = learn_mac_addresses()
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
@@ -32,7 +50,7 @@ def discover():
             host = dict()
             host["name"] = hostname[0]
             host["ip_address"] = str(ip_addr)
-            host["mac_address"] = ""
+            host["mac_address"] = mac_addresses[str(ip_addr)] if str(ip_addr) in mac_addresses else ""
             host["availability"] = True
             host["response_time"] = 1.0
             host["last_heard"] = str(datetime.now())
