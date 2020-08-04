4f0e791855b6580eb026f87198496fa37adda062
chuck <chuck.a.black@gmail.com>
2020-08-04 10:35:57 -0700

    changing .env to have standard <quokka-ip>

--- run-ui.sh
+++ run-ui.sh
@@ -1,6 +1,8 @@
 ip=$(hostname -I)
 echo starting quokka-ui with host IP address: ${ip}
-sed -i "s/192.168.254.125/${ip}/" quokka-ui/.env
+sed -i "s/<quokka-ip>/${ip}/" quokka-ui/.env
 cat quokka-ui/.env
 cd ~/quokka/quokka-ui
 npm start
+sed -i "s/${ip}/<quokka-ip>/" .env
+cat .env
