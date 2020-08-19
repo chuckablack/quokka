480bf4c81ee731e42867783a5119c146d2558d1d
chuck black <chuck.a.black@gmail.com>
2020-08-19 10:19:42 -0700

    improving run-ui to use env-bak

--- run-ui.sh
+++ run-ui.sh
@@ -1,8 +1,7 @@
 ip=$(hostname -I)
 echo starting quokka-ui with host IP address: ${ip}
+cp quokka-ui/.env-bak quokka-ui/.env
 sed -i "s/<quokka-ip>/${ip}/" quokka-ui/.env
 cat quokka-ui/.env
 cd ~/quokka/quokka-ui
 npm start
-sed -i "s/${ip}/<quokka-ip>/" .env
-cat .env
