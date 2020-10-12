1c251c98259fb81b52031e614c50e01134c87909
chuck black <chuck.a.black@gmail.com>
2020-10-12 12:01:09 -0700

    removed old code and new code constants for testing from devices.js

--- quokka-ui/src/components/Devices.js
+++ quokka-ui/src/components/Devices.js
@@ -12,25 +12,6 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
 import ReactDiffViewer from 'react-diff-viewer'
 
-const oldCode = `
-const a = 10
-const b = 10
-const c = () => console.log('foo')
-
-if(a > 10) {
-  console.log('bar')
-}
-
-console.log('done')
-`;
-const newCode = `
-const a = 10
-const boo = 10
-
-if(a === 10) {
-  console.log('bar')
-}
-`;
 class Devices extends Component {
 
     constructor(props) {
